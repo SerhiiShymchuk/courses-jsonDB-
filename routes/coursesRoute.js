d9cc5dee112d2991908c18d746b404ae1cb160db
SerhiiShymchuk <shymaser@gmail.com>
2021-07-12 13:06:17 +0300

    атворизація, розділення ролей, захист форм

--- routes/coursesRoute.js
+++ routes/coursesRoute.js
@@ -8,48 +8,72 @@ const authProtect = require('../middleware/authProtect')
 router.get('', async (req, res) => {
     //const course = new Course()
     //const arrCourses = await course.getAll()
-    const courses = await Course.find()
-    res.render('courses', {
-        title: 'Список курсів ',
-        iscourses: true,
-        //arrCourses,
-        courses,
-    })
+    try {
+        const courses = await Course.find()
+                .populate('userId', 'email name')
+                .select('price title img')
+        res.render('courses', {
+            title: 'Список курсів ',
+            iscourses: true,
+            userId: req.user ? req.user._id.toString() : null,
+            //arrCourses,
+            courses,
+        })
+    } catch (err) {
+        throw err
+    }
 })
 router.get('/:id', async (req, res) => {
     // const courses = new Course()
     // const course = await courses.getById(req.params.id)
-    const course = await Course.findById(req.params.id)
-    res.render('course', {
-        title: `Курс ${course.title}`,
-        layout: 'empty',
-        course,
-    })
+    try {
+        const course = await Course.findById(req.params.id)
+        res.render('course', {
+            title: `Курс ${course.title}`,
+            layout: 'empty',
+            course,
+        })
+    } catch (err) {
+        throw err
+    }
 })
 router.get('/:id/edit', authProtect, async (req, res) => {
     if (!req.query.allow) res.redirect('/')
 
     // const courses = new Course()
     // const course = await courses.getById(req.params.id)
-    const course = await Course.findById(req.params.id)
-    res.render('course-edit', {
-        title: `Редагувати ${course.title}`,
-        course,
-    })
+
+    try {
+        const course = await Course.findById(req.params.id)
+        if (course.userId.toString() !== req.user._id.toString()) return res.redirect('/courses')
+        res.render('course-edit', {
+            title: `Редагувати ${course.title}`,
+            course,
+        })
+    } catch (err) {
+        throw err
+    }
 })
 router.post('/:id/edit', authProtect, async (req, res) => {
     // const course = new Course()
     // console.log(`---- req.body ----`)
     // console.log(req.body)
     //await course.update(req.body)
-    const id = req.body.id
-    delete req.body.id
-    await Course.findByIdAndUpdate(id, req.body)
-    res.redirect('/courses')
+    try {
+        const id = req.body.id
+        delete req.body.id
+        await Course.findByIdAndUpdate(id, req.body)
+        res.redirect('/courses')
+    } catch (err) {
+        throw err
+    }
 })
 router.post('/remove', authProtect, async (req, res) => {
     try {
-        await Course.deleteOne({ _id: new ObjectId(req.body.id) })
+        await Course.deleteOne({
+            _id: new ObjectId(req.body.id),
+            userId: req.user._id,
+        })
         res.redirect('/courses')
     } catch (error) { throw error }
 })
