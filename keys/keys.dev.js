9bc2af100ea71cbc4b17d67896e98302deaf0479
SerhiiShymchuk <shymaser@gmail.com>
2021-07-14 13:44:56 +0300

    keys

--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -5,6 +5,6 @@ module.exports = {
     url: `mongodb+srv://admin:85hevFHBVxIqZ2tp@cluster0.hdehi.mongodb.net/shop`,
     secret: 'some text',
     sendgrid_email_api_key: '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',
-    base_url: 'http://localhost:3009',
+    base_url: `http://localhost:${process.env.PORT}`,
     email_from: 'shymakachok@mail.ru',
 }
